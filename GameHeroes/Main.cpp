0490fae0b61aa576ef5bab86b12d787a5d7cea22
Pavel035 <84439733+Pavel035@users.noreply.github.com>
2023-06-18 19:48:15 +0100

    Add files via upload

--- GameHeroes/Main.cpp
+++ GameHeroes/Main.cpp
@@ -5,6 +5,7 @@
 MyString and Vector are from Github - Georgi Terziev with modifications for work with binary files
 For using test data u should use load command as admin
 All commands can be shown by using help command
+Github: https://github.com/Pavel035/cpp_projects/tree/579b03472247f9c2d7ba9bb33f4c45bd4e954de9/GameHeroes
 */
 
 
